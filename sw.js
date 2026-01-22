d9cd8da42a6e0103cced3f195131c4c8eaa14153
mlbbxkitty-png <mlbbxkitty@gmail.com>
2026-01-22 10:57:14 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("draw-cache").then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
